9f33712bbc156012e6477fef4688f592757eb429
PArts23 <alonso2405.13@gmail.com>
2022-11-18 01:39:54 -0500

    Se implementaron los querys de busqueda 'Numero de solicitudes por artesano' y 'Solicitudes mas recientes' para la entidad Solicitud-disenio

--- CreateDev/src/app/service/solicitud-disenio.service.ts
+++ CreateDev/src/app/service/solicitud-disenio.service.ts
@@ -1,3 +1,5 @@
+import { SolicitudesArtesano } from './../model/solicitudes-artesano';
+import { SolicitudesRecientes } from './../model/solicitudes-recientes';
 import { solicitud_disenio } from './../model/solicitud-disenio';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -40,5 +42,11 @@ export class SolicitudDisenioService {
   }
   setConfirmaEliminacion(estado: Boolean) {
     this.confirmaEliminacion.next(estado);
+  }
+  buscarSolicitudesArtesanos() {
+    return this.http.get<SolicitudesArtesano[]>(`${this.url}/solicitudes-artesanos`);
+  }
+  buscarSolicitudesRecientes() {
+    return this.http.get<SolicitudesRecientes[]>(`${this.url}/solicitudes-mas-recientes`);
   }
 }
