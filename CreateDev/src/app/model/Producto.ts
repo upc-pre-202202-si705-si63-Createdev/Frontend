3e637b6fc44a85e63c729a12e5c7d2c2499743eb
diiegoricse <u202016793@upc.edu.pe>
2022-11-19 14:03:58 -0500

    fix: Entity Producto can create a data by frontend to backend

--- CreateDev/src/app/model/Producto.ts
+++ CreateDev/src/app/model/Producto.ts
@@ -2,15 +2,15 @@ import { Tipo_Producto } from './tipo-producto';
 import { Artesano } from './Artesanos';
 
 export class Producto {
-    id: number = 0;
-    nombre: string = "";
-    descripcion: string = ""
-    stock: number = 0;
-    peso: number = 0;
-    precio_unitario: number = 0;
-    material: string = "";
-    fecha_creacion: string = "";
-    lugar_fabricacion: string = "";
-    tipo_producto: Tipo_Producto = new Tipo_Producto();
-    artesano: Artesano = new Artesano();
+    idProducto: number = 0;
+    nameProducto: string = "";
+    descripcionProducto: string = ""
+    stockProducto: number = 0;
+    pesoProducto: number = 0;
+    precioProducto: number = 0;
+    materialProducto: string = "";
+    fechaProducto: string = "";
+    lugarfabricacionProducto: string = "";
+    tipoproductoProducto: Tipo_Producto = new Tipo_Producto();
+    artesanoProducto: Artesano = new Artesano();
 }
